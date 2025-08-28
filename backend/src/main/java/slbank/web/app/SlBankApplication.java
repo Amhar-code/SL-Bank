1c47b791447b4f6fb43ec8a51659be9e3894fe3b
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-08-28 22:17:19 +0530

    created the springboot structure

---
+++ backend/src/main/java/slbank/web/app/SlBankApplication.java
@@ -0,0 +1,13 @@
+package slbank.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlBankApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/slbank/web/app/SlBankApplicationTests.java
@@ -0,0 +1,13 @@
+package slbank.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
