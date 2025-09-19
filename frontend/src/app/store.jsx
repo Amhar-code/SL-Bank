83d31db12c973c017cce80511c590f9b46d05207
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-09-19 10:46:50 +0530

    Developed the card function User Interface

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice';
 import pageReducer from '../features/page/pageSlice'; 
 import accountReducer from '../features/account/accountSlice';  
+import cardReducer from "../features/card/cardSlice"
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     pages: pageReducer, 
-    account: accountReducer,  
+    account: accountReducer,
+    card: cardReducer,    
   },
 });
